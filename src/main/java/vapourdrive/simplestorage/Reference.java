f3d56643352ed6c2a117060c1cd878f22d9b0dd5
VapourDrive <colin.g.bos@gmail.com>
2015-09-16 18:25:18 -0400

    Updated Version

--- src/main/java/vapourdrive/simplestorage/Reference.java
+++ src/main/java/vapourdrive/simplestorage/Reference.java
@@ -4,6 +4,6 @@ public class Reference
 {
 	public static final String ModID = "simplestorage";
 	public static final String Name = "Simple Storage";
-	public static final String Version = "0.0.2a.3";
+	public static final String Version = "0.0.3.4";
 	public static final String ResourcePath = "simplestorage:";
 }
