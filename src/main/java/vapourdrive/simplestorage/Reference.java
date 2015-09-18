1180e6f2ddda88895d14a7285d482bea092d4c8d
VapourDrive <colin.g.bos@gmail.com>
2015-09-18 11:24:57 -0400

    Version bump

--- src/main/java/vapourdrive/simplestorage/Reference.java
+++ src/main/java/vapourdrive/simplestorage/Reference.java
@@ -4,6 +4,6 @@ public class Reference
 {
 	public static final String ModID = "simplestorage";
 	public static final String Name = "Simple Storage";
-	public static final String Version = "0.1.4e.10";
+	public static final String Version = "0.1.5.11";
 	public static final String ResourcePath = "simplestorage:";
 }
